657d9c687f6bc585cac244fecc18eb721479686a
Gid Baerridge <gid.berridge@gmail.com>
2021-10-21 20:00:33 +0100

    skills component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from "react";
-import Header from "./JS-components/header";
+import Header from "./JS-components/header.jsx";
+import Skills from "./JS-components/skills.jsx";
 import "./sass/index.scss";
 
 const App = () => {
   return (
     <div className="container">
       <Header />
+      <Skills />
     </div>
   );
 };
