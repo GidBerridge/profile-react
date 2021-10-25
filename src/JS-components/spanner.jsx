e67e88d164e0d938d7581247e3dbf6d6b7b7b3cc
Gid Baerridge <gid.berridge@gmail.com>
2021-10-25 16:51:40 +0100

    projects text adjusted

--- src/JS-components/spanner.jsx
+++ src/JS-components/spanner.jsx
@@ -10,7 +10,9 @@ class Spanner extends Component {
           <div>
             <h2 className="card-project-infos title">www.spannerhq.com</h2>
             <h2 className="card-project-infos">Wordpress</h2>
-            <p>Short description here!</p>
+            <p>A Wordpress site using Elementor theme builder.</p>
+            <p>The site is a central site for the many music acts associated with Dan Spanner</p>
+            <p>and contains links to all the associated social media.</p>
           </div>
         </div>
       </div>
